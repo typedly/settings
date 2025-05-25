a2de7cff45c63330591f0f3f2ca692a13115a8b9
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-25 15:26:20 +0000

    refactor(`PatternSettings`): change the `Pattern` to `Value` and  use it as exact pattern.

--- src/interface/pattern-settings.interface.ts
+++ src/interface/pattern-settings.interface.ts
@@ -1,8 +1,15 @@
+// Interface.
 import { PatternOptions } from "./pattern-options.interface";
-
+/**
+ * @description Represents the settings for pattern validation.
+ * @export
+ * @interface PatternSettings
+ * @template {RegExp} [Value=RegExp] The type of the regular expression pattern.
+ * @template {object} [Options=PatternOptions<Value>] The type of the options for the pattern as enhanced customization.
+ */
 export interface PatternSettings<
-  Pattern extends RegExp = RegExp,
-  Options extends object = PatternOptions<Pattern>
+  Value extends RegExp = RegExp,
+  Options extends object = PatternOptions<Value>
 > {
-  pattern: Options
+  pattern: Value | Options
 }
