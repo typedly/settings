a1ceffb9dd65acd13559f3b2ae6877bddaa66a4e
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-25 15:20:52 +0000

    refactor(`LengthOptions`): add exact `value` also as between `min` and `max`.

--- src/interface/length-options.interface.ts
+++ src/interface/length-options.interface.ts
@@ -1,7 +1,17 @@
+/**
+ * @description Represents options for length validation.
+ * @export
+ * @interface Length
+ * @template {number} [Value=number] The expected length of the value, also between min and max.
+ * @template {number} [Min=number] The minimum length of the value.
+ * @template {number} [Max=number] The maximum length of the value.
+ */
 export interface LengthOptions<
+  Value extends number = number,
   Min extends number = number,
   Max extends number = number
 > {
+  value?: Value;
   min?: Min;
   max?: Max;
 }
