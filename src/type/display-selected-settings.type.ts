4a38c7c874eaa2f3f98b5d5f85b967d6e7ba475c
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-25 00:23:09 +0000

    docs(`DisplaySelectedSettings`): update.

--- src/type/display-selected-settings.type.ts
+++ src/type/display-selected-settings.type.ts
@@ -1,4 +1,9 @@
-
+/**
+ * @description The type is used to create a new type that includes only the properties of the original type `T` specified in the tuple `K`.
+ * @export
+ * @template {readonly (keyof T)[]} K The tuple of keys to select from the type `T`.
+ * @template T 
+ */
 export type DisplaySelectedSettings<
   K extends readonly (keyof T)[],
   T
