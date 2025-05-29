58926d9b4dafb14863b6422a984c3c288361bd89
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-29 17:34:13 +0000

    refactor(`PatternSettings`): define properties with `undefined`.

--- src/interface/pattern-settings.interface.ts
+++ src/interface/pattern-settings.interface.ts
@@ -1,15 +1,37 @@
-// Interface.
-import { PatternOptions } from "./pattern-options.interface";
 /**
- * @description Represents the settings for pattern validation.
+ * @description Represents the pattern settings for validation.
  * @export
  * @interface PatternSettings
- * @template {RegExp} [Value=RegExp] The type of the regular expression pattern.
- * @template {object} [Options=PatternOptions<Value>] The type of the options for the pattern as enhanced customization.
+ * @template {RegExp} [Value=RegExp | undefined] The regular expression pattern to match.
  */
-export interface PatternSettings<
-  Value extends RegExp = RegExp,
-  Options extends object = PatternOptions<Value>
-> {
-  pattern: Value | Options
-}
+export interface PatternSettings<Value extends RegExp | undefined = RegExp> {
+  /**
+   * @description Represents the regular expression pattern to match.
+   * @type {Value}
+   */
+  regexp: Value;
+  
+  /**
+   * @description Represents the lowercase option for the pattern validation.
+   * @type {boolean}
+   */
+  lowercase: boolean;
+
+  /**
+   * @description Represents the uppercase option for the pattern validation.
+   * @type {boolean}
+   */
+  uppercase: boolean;
+
+  /**
+   * @description Represents the numeric option for the pattern validation.
+   * @type {boolean}
+   */
+  numeric: boolean;
+
+  /**
+   * @description Represents the special option for the pattern validation.
+   * @type {boolean}
+   */
+  special: boolean;
+};
