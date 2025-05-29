d68b7b4411cbb6a9fbd3f3c9081e7274f55da3f0
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-29 17:37:35 +0000

    chore(type/index): update.

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,2 +1,4 @@
-export type { SelectableSettings } from './selectable-settings.type';
 export type { DisplaySelectedSettings } from './display-selected-settings.type';
+export type { OptionalField } from './optional-field.type';
+export type { RequiredField } from './required-field.type';
+export type { SelectableSettings } from './selectable-settings.type';
