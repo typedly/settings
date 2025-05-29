b1f11034d2dfad188a328a41ffb3fb91c58d5927
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-29 17:58:19 +0000

    refactor(`Settings`): update import locations.

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,12 +1,7 @@
 // Length.
-export type { Length } from './length.interface';
-export type { LengthOptions } from './length-options.interface';
-export type { LengthSetting } from './length-setting.interface';
-export type { LengthSettings } from './length-settings.interface';
+export type { Length, LengthOptions, LengthSetting, LengthSettings } from './length';
 // Pattern.
-export type { PatternOptions } from './pattern-options.interface';
-export type { PatternSetting } from './pattern-setting.interface';
-export type { PatternSettings } from './pattern-settings.interface';
+export type { PatternOptions, PatternSetting, PatternSettings } from './pattern';
 // Settings.
 export type { Settings } from './settings.interface';
 export type { ValueSetting } from './value-setting.interface';
--- src/interface/settings.interface.ts
+++ src/interface/settings.interface.ts
@@ -1,6 +1,6 @@
 // Interface.
-import { LengthSetting } from "./length-setting.interface";
-import { PatternSetting } from "./pattern-setting.interface";
+import { LengthSetting } from "./length";
+import { PatternSetting } from "./pattern";
 import { ValueSetting } from "./value-setting.interface";
 /**
  * @description Represents the settings for a configuration that includes value, length, and pattern validation.
