d754cb3dd40c65f80e6e04df6e1564fab1d5b90b
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-29 17:35:31 +0000

    refactor(`Settings`): use singular names, and `undefined`.

--- src/interface/settings.interface.ts
+++ src/interface/settings.interface.ts
@@ -1,28 +1,28 @@
 // Interface.
-import { LengthSettings } from "./length-settings.interface";
-import { PatternSettings } from "./pattern-settings.interface";
-import { ValueSettings } from "./value-settings.interface";
+import { LengthSetting } from "./length-setting.interface";
+import { PatternSetting } from "./pattern-setting.interface";
+import { ValueSetting } from "./value-settings.interface";
 /**
  * @description Represents the settings for a configuration that includes value, length, and pattern validation.
  * @export
  * @interface Settings
  * @template [Value=string] 
- * @template {number} [Length=number] The type of the expected length of the value, also between min and max.
- * @template {number} [Min=number] The type of the minimum length of the value.
- * @template {number} [Max=number] The type of the maximum length of the value.
- * @template {RegExp} [Pattern=RegExp] The type of pattern to match against the value.
- * @extends {ValueSettings<Value>} Extends the ValueSettings interface for value validation.
- * @extends {LengthSettings<Min, Max>} Extends the LengthSettings interface for length validation.
- * @extends {PatternSettings<Pattern>} Extends the PatternSettings interface for pattern validation.
+ * @template {number} [Length=number | undefined] The type of the expected length of the value, also between min and max.
+ * @template {number} [Min=number | undefined] The type of the minimum length of the value.
+ * @template {number} [Max=number | undefined] The type of the maximum length of the value.
+ * @template {RegExp} [Pattern=RegExp | undefined] The type of pattern to match against the value.
+ * @extends {ValueSetting<Value>} Extends the ValueSettings interface for value validation.
+ * @extends {LengthSetting<Min, Max>} Extends the LengthSettings interface for length validation.
+ * @extends {PatternSetting<Pattern>} Extends the PatternSettings interface for pattern validation.
  */
 export interface Settings<
   Value = string,
-  Length extends number = number,
-  Min extends number = number,
-  Max extends number = number,
-  Pattern extends RegExp = RegExp
+  Length extends number | undefined = number,
+  Min extends number | undefined = number,
+  Max extends number | undefined = number,
+  Pattern extends RegExp | undefined = RegExp
 > extends
-  ValueSettings<Value>,
-  LengthSettings<Length, Min, Max>,
-  PatternSettings<Pattern> {
+  ValueSetting<Value>,
+  LengthSetting<Length, Min, Max>,
+  PatternSetting<Pattern> {
 }
