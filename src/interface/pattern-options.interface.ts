e8605295c7f9e7af2923b6d4cadeea619b734b67
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-29 17:33:04 +0000

    refactor(`PatternOptions`): use `PatternSettings`, and use `undefined`.

--- src/interface/pattern-options.interface.ts
+++ src/interface/pattern-options.interface.ts
@@ -1,37 +1,11 @@
+// Interface.
+import { PatternSettings } from "./pattern-settings.interface";
+// Type.
+import { OptionalField } from '../type/optional-field.type';
 /**
  * @description Represents the pattern options for validation settings.
  * @export
  * @interface PatternOptions
- * @template {RegExp} [Value=RegExp] The regular expression pattern to match.
+ * @template {RegExp} [Value=RegExp | undefined] The regular expression pattern to match.
  */
-export interface PatternOptions<Value extends RegExp = RegExp> {
-  /**
-   * @description Represents the regular expression pattern to match.
-   * @type {?Value}
-   */
-  regexp?: Value;
-  
-  /**
-   * @description Represents the lowercase option for the pattern validation.
-   * @type {?boolean}
-   */
-  lowercase?: boolean;
-
-  /**
-   * @description Represents the uppercase option for the pattern validation.
-   * @type {?boolean}
-   */
-  uppercase?: boolean;
-
-  /**
-   * @description Represents the numeric option for the pattern validation.
-   * @type {?boolean}
-   */
-  numeric?: boolean;
-
-  /**
-   * @description Represents the special option for the pattern validation.
-   * @type {?boolean}
-   */
-  special?: boolean;
-};
+export interface PatternOptions<Value extends RegExp | undefined = RegExp> extends OptionalField<PatternSettings<Value>> {}
