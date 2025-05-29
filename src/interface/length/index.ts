c9155c56d58324ef1585c625575ddf470c0ae0ca
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-29 17:56:48 +0000

    chore(length):  move interfaces to the length.

---
+++ src/interface/length/index.ts
@@ -0,0 +1,4 @@
+export type { Length } from './length.interface';
+export type { LengthOptions } from './length-options.interface';
+export type { LengthSetting } from './length-setting.interface';
+export type { LengthSettings } from './length-settings.interface';
