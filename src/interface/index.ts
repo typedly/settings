1ab847c221965f5efeab20e951c7b5425efeee5b
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-06-01 04:21:50 +0000

    chore(interface/index): remove unnecessary.

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,7 +1,3 @@
-// Length.
-export type { Length, LengthOptions, LengthSetting, LengthSettings } from './length';
-// Pattern.
-export type { PatternOptions, PatternSetting, PatternSettings } from './pattern';
 // Settings.
 export type { Settings } from './settings.interface';
 export type { ValueSetting } from './value-setting.interface';
